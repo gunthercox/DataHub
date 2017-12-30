95af40b99f227734cbe330ef802f4cdb9af51b78
Gunther Cox <gunthercx@gmail.com>
2017-12-30 18:34:24 -0500

    Create simple api

---
+++ app/datafeed/main.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask import jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    value = db.Column(db.String(120))
+    expires = db.Column(db.DateTime(timezone=True), nullable=True)
+
+    def __repr__(self):
+        return '<Event %r>' % self.name
+
+def serialize_event(event):
+    return {
+        'id': event.id,
+        'name': event.name,
+        'value': event.value,
+        'expires': event.expires
+    }
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'POST':
+
+        data = request.get_json()
+        errors = []
+
+        if 'name' not in data:
+            errors.append('The field "name" is required.')
+
+        if 'value' not in data:
+            errors.append('The field "value" is required.')
+
+        if errors:
+            return jsonify(errors=errors), 400
+
+        event = Event(
+            name=data.get('name'),
+            value=data.get('value'),
+            expires=data.get('expires')
+        )
+
+        db.session.add(event)
+        db.session.commit()
+
+        return jsonify(serialize_event(event)), 200
+
+    # Get the last 100 events
+    events = Event.query.order_by(Event.id.desc()).limit(100)
+
+    return jsonify([
+        serialize_event(e) for e in events
+    ])
---
+++ app/tests/test_api.py
@@ -0,0 +1,113 @@
+import json
+from unittest import TestCase
+from datafeed.main import app, db, Event
+
+
+class ApiTestCase(TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+        self.endpoint = '/'
+
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def assertJSON(self, json_data, dict_data):
+        """
+        Assert that JSON data is equal to a dictionary of data.
+        """
+        import json
+
+        data = json.loads(json_data)
+        self.assertEqual(data, dict_data)
+
+    def test_get_empty_list(self):
+        response = self.app.get(self.endpoint)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertJSON(response.data, [])
+
+    def test_get_list(self):
+        for i in range(1, 4):
+            event = Event(
+                name='Name %s' % i,
+                value=i
+            )
+            db.session.add(event)
+        db.session.commit()
+
+        response = self.app.get(self.endpoint)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertJSON(response.data, [
+            {'expires': None, 'id': 3, 'name': 'Name 3', 'value': '3'},
+            {'expires': None, 'id': 2, 'name': 'Name 2', 'value': '2'},
+            {'expires': None, 'id': 1, 'name': 'Name 1', 'value': '1'}
+        ])
+
+    def test_post_valid_event(self):
+        response = self.app.post(
+            self.endpoint,
+            data=json.dumps({
+                'name': 'test',
+                'value': 100
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertJSON(response.data, {
+            'id': 1,
+            'name': 'test',
+            'value': '100',
+            'expires': None
+        })
+
+        events = Event.query.filter_by(name='test', value=100)
+        self.assertEqual(events.count(), 1)
+
+        event = events.first()
+        self.assertEqual(event.expires, None)
+
+    def test_post_valid_event_with_expiration(self):
+        pass
+
+    def test_post_event_without_name(self):
+        response = self.app.post(
+            self.endpoint,
+            data=json.dumps({
+                'value': 100
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertJSON(response.data, {
+            'errors': [
+                'The field "name" is required.' 
+            ]
+        })
+
+        self.assertEqual(Event.query.count(), 0)
+
+    def test_post_event_without_value(self):
+        response = self.app.post(
+            self.endpoint,
+            data=json.dumps({
+                'name': 'test'
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertJSON(response.data, {
+            'errors': [
+                'The field "value" is required.'
+            ]
+        })
+
+        self.assertEqual(Event.query.count(), 0)
